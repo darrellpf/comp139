683c490a0baf0809d177d974cd0140816ebfc942
darrell <darrell.camosun@gmail.com>
2014-04-30 15:43:38 -0700

    Sample solution, including two ways of doing addAll

--- Lab3/src/chainedlist/ChainedList.java
+++ Lab3/src/chainedlist/ChainedList.java
@@ -69,11 +69,101 @@ public class ChainedList<E> implements GenericList<E> {
 	}
 
 	public void addAll(ChainedList<E> other) {
-		
+		if (other.itemCount == 0) {
+			return;
+		}
+
+		Node dummy = null;
+
+		Node currentOther = other.startOfChain;
+		Node tail = startOfChain;
+
+		if (startOfChain == null) {
+			dummy = new Node(null);
+			tail = dummy;
+		}
+
+		while (tail.next != null) {
+			tail = tail.next;
+		}
+
+		while (currentOther != null) {
+			tail.next = new Node(currentOther.storedItem);
+			tail = tail.next;
+
+			currentOther = currentOther.next;
+		}
+
+		if (dummy != null) {
+			startOfChain = dummy.next;
+		}
+
+		itemCount += other.itemCount;
+	}
+
+	public void addAllNoDummy(ChainedList<E> other) {
+		if (other.itemCount == 0) {
+			return;
+		}
+
+		Node copyHead = new Node(other.startOfChain.storedItem);
+		Node copyTail = copyHead;
+		Node copyThis = other.startOfChain.next;
+
+		while (copyThis != null) {
+			copyTail.next = new Node(copyThis.storedItem);
+
+			copyTail = copyTail.next;
+			copyThis = copyThis.next;
+		}
+
+		itemCount += other.itemCount;
+
+		if (startOfChain == null) {
+			startOfChain = copyHead;
+			return;
+		}
+
+		Node end = startOfChain;
+
+		while (end.next != null) {
+			end = end.next;
+		}
+
+		end.next = copyHead;
 	}
 
 	public ChainedList<E> addMarker(E match, E marker) {
-		return null;
+		ChainedList<E> result = new ChainedList<E>();
+
+		result.startOfChain = new Node(null); // add dummy node
+		Node tail = result.startOfChain;
+
+		Node nextMatch = startOfChain;
+
+		while (nextMatch != null) {
+			Node newNode = new Node(nextMatch.storedItem);
+
+			if (nextMatch.storedItem.equals(match)) {
+				Node markerNode = new Node(marker);
+
+				tail.next = markerNode;
+				tail = tail.next;
+
+				result.itemCount++;
+			}
+
+			tail.next = newNode;
+			tail = tail.next;
+
+			result.itemCount++;
+
+			nextMatch = nextMatch.next;
+		}
+
+		result.startOfChain = result.startOfChain.next; // remove dummy node
+
+		return result;
 	}
 
 	private class Node {
